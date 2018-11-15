12588ff0de539a724c339148b612b1abf534220c
NobbsNobby <art.kotov1@gmail.com>
2018-11-15 12:06:33 +0300

    add image-minify

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
 
 module.exports = {
   entry: './src/js/index.js',
@@ -47,22 +47,28 @@ module.exports = {
       filename: '../css/style.min.css'
     }),
     new CopyWebpackPlugin([
-          {
-            from: 'src/fonts',
-            to: path.resolve(__dirname, 'dist/fonts'),
-            cache: true
-          },
-          {
-            from: 'src/img',
-            to: path.resolve(__dirname, 'dist/img'),
-            cache: true
-          },
-          {
-            from: 'src/index.html',
-            to: path.resolve(__dirname, 'dist'),
-            toType: 'dir'
-          }
-        ])
+      {
+        from: 'src/fonts',
+        to: path.resolve(__dirname, 'dist/fonts'),
+        cache: true
+      },
+      {
+        from: 'src/img',
+        to: path.resolve(__dirname, 'dist/img'),
+        cache: true
+      },
+      {
+        from: 'src/favicon',
+        to: path.resolve(__dirname, 'dist/favicon'),
+        cache: true
+      },
+      {
+        from: 'src/index.html',
+        to: path.resolve(__dirname, 'dist'),
+        toType: 'dir'
+      }
+    ]),
+    new ImageminPlugin({ test: /\.(jpe?g|png|gif|svg)$/i })
   ],
   output: {
     filename: 'main.js',
